0d2aea48698368ce6c711795abc22cb218147dd9
Adithya Vijay K <adithyavj@gmail.com>
2022-06-02 22:06:01 +0530

    Add basic design to home, call to fetch covid api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class="home">
-    Hello world!
-  </div>
+  <main v-if="!loading">
+    show data
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="Loading image" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
 
 export default {
   name: 'HomeView',
-  components: {
-    
+  components: { },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      date: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+    this.date = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
   }
 }
 </script>
